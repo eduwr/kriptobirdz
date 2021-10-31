7222db8b248a5831a3f0083b60ca30d9bcb7b91c
eduwr <eduwric@gmail.com>
2021-10-31 13:12:12 -0300

    config: hardhat configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
 const projectId = "9fdcfe08c3404068964dc7b5e115e1c2";
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
+
+const keyData = fs.readFileSync("./p-key.txt", {
+  encoding: "utf8",
+  flag: "r",
+});
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -10,15 +16,15 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337, // onfig standard
+      chainId: 1337, // config standard
     },
     mumbai: {
       url: "https://polygon-mumbai.infura.io/v3/" + projectId,
-      accounts: [],
+      accounts: [keyData],
     },
     mainnet: {
       url: "https://polygon-mainnet.infura.io/v3/" + projectId,
-      accounts: [],
+      accounts: [keyData],
     },
   },
   solidity: {
